0bc27f2e94a53731978dce2d29c917b105a16226
Bart Trojanowski <bart@jukie.net>
2008-09-07 00:42:13 -0400

    move file rewrite into a separate function

--- neg_load_rsvg.c
+++ neg_load_rsvg.c
@@ -18,6 +18,39 @@
 #define DISPLAY_INLINE "display:inline"
 
 
+static void rewrite_display_inline(char *in, char *end, int fd_out)
+{
+	char *p;
+	pcre *re;
+	const char *error;
+	int erroffset;
+	int rc;
+	int ovector[OVECCOUNT];
+
+	re = pcre_compile(DISPLAY_NONE, 0, &error, &erroffset, NULL);
+	if (!re)
+		errx(1, "Internal regular expression error at %u: %s.",
+				erroffset, error);
+
+	for (p=in; p<end; ) {
+
+		rc = pcre_exec(re, NULL, p, end-p, 0, 0, ovector, OVECCOUNT);
+		if (rc == PCRE_ERROR_NOMATCH)
+			break;
+		if (rc <= 0)
+			errx(1, "Regular expression error %d.", rc);
+
+		write(fd_out, p, ovector[0]);
+		write(fd_out, DISPLAY_INLINE, strlen(DISPLAY_INLINE));
+
+		p += ovector[0] + strlen(DISPLAY_NONE);
+	}
+
+	pcre_free(re);
+
+	write(fd_out, p, end-p);
+}
+
 RsvgHandle *neg_load_rsvg(const char *name)
 {
 	GError *err;
@@ -27,11 +60,7 @@ RsvgHandle *neg_load_rsvg(const char *name)
 	int fd_tmp;
 	char tempfilename[] = "/tmp/negative-XXXXXX";
 	int rc;
-	char *inmm, *p, *end;
-	pcre *re;
-	const char *error;
-	int erroffset;
-	int ovector[OVECCOUNT];
+	char *inmm, *end;
 
 	fd_in = open(name, O_RDONLY);
 	if (fd_in < 0)
@@ -55,28 +84,7 @@ RsvgHandle *neg_load_rsvg(const char *name)
 		errx(1, "Could not create a temporary file in /tmp/: %s",
 				strerror(errno));
 
-	re = pcre_compile(DISPLAY_NONE, 0, &error, &erroffset, NULL);
-	if (!re)
-		errx(1, "Internal regular expression error at %u: %s.",
-				erroffset, error);
-
-	for (p=inmm; p<end; ) {
-
-		rc = pcre_exec(re, NULL, p, end-p, 0, 0, ovector, OVECCOUNT);
-		if (rc == PCRE_ERROR_NOMATCH)
-			break;
-		if (rc <= 0)
-			errx(1, "Regular expression error %d.", rc);
-
-		write(fd_tmp, p, ovector[0]);
-		write(fd_tmp, DISPLAY_INLINE, strlen(DISPLAY_INLINE));
-
-		p += ovector[0] + strlen(DISPLAY_NONE);
-	}
-
-	pcre_free(re);
-
-	write(fd_tmp, p, end-p);
+	rewrite_display_inline(inmm, end, fd_tmp);
 	close(fd_tmp);
 
 	rsvg_init();
